01b27bdf88d9041dd4dea62239de310581641cb6
rondoudou78 <83508285+rondoudou78@users.noreply.github.com>
2021-05-01 18:12:39 +0200

    initail commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+//Toutes les actions à faire quand le bot se connecte
+client.on("ready", function () {
+    console.log("Mon BOT est Connecté");
+})
+
+// Répondre à un message
+client.on("message", function (message) {
+    if (message.content === "pute") { // Lorsque "Salut" est envoyé
+        message.channel.send("@[NRGS]⚜Rondoudou_78⚜ est mega bg")
+    }
+})
+
+client.login(ODAwNDI4NjgyOTQyMjE4MzIw.YAR_TQ.CDF_vKzQHBDanySCVtTkUH0_8rg);
